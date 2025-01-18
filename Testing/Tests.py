2af42205f04bd932e9302b47fe0779acd52c7c34
ColeMinersUnion <colehansen018@gmail.com>
2025-01-18 12:00:43 -0500

    Had to move the environment file to the root directory to get the tests to run properly.

--- Testing/Tests.py
+++ Testing/Tests.py
@@ -1,9 +1,11 @@
 import requests
 
-def test_get():
+
+def test_response():
     response = requests.get('http://localhost:8080/')
-    print(response.text)
-    #assert response.status_code == 200
+    assert response.status_code == 200
+    assert response.text == '<!DOCTYPE html><html><body><h1>Hello World</h1></body></html>'
+
 
 if __name__ == '__main__':
-    test_get()
+    test_response()
